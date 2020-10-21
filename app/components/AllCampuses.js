a9114c0173c7578a9d969debe466c473b2b5dea8
Valentyna Vasylieva <smithaddaf@gmail.com>
2020-10-21 15:11:17 -0500

    Feature: Get All Campuses and Students

--- app/components/AllCampuses.js
+++ app/components/AllCampuses.js
@@ -5,6 +5,7 @@ import { fetchCampuses } from "../redux/campuses"
 // Notice that we're exporting the AllCampuses component twice. The named export
 // (below) is not connected to Redux, while the default export (at the very
 // bottom) is connected to Redux. Our tests should cover _both_ cases.
+
 export class AllCampuses extends React.Component {
   componentDidMount (){
     this.props.getCampuses();
